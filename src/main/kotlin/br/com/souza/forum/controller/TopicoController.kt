3fcefcd248770b2beb836e7b0098e33e83eff3ac
Rubens G Souza <rubensgoncalvessouza@gmail.com>
2022-02-12 18:09:56 -0300

    Adicionando cache

--- src/main/kotlin/br/com/souza/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/souza/forum/controller/TopicoController.kt
@@ -4,8 +4,11 @@ import br.com.souza.forum.dto.AtualizacaoTopicoForm
 import br.com.souza.forum.dto.NovoTopicoForm
 import br.com.souza.forum.dto.TopicoView
 import br.com.souza.forum.service.TopicoService
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -19,9 +22,10 @@ import javax.validation.Valid
 class TopicoController(private val topicoService: TopicoService) {
 
     @GetMapping
+    @Cacheable("topicos")
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
-        @PageableDefault(size = 5) paginacao: Pageable
+        @PageableDefault(size = 5, sort = ["dataCriacao"], direction = Sort.Direction.DESC) paginacao: Pageable
     ): Page<TopicoView> {
         return topicoService.listar(nomeCurso, paginacao)
     }
@@ -33,6 +37,7 @@ class TopicoController(private val topicoService: TopicoService) {
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = ["topicos"], allEntries = true)
     fun cadastrar(
         @RequestBody @Valid form: NovoTopicoForm,
         uriBuilder: UriComponentsBuilder
@@ -44,6 +49,7 @@ class TopicoController(private val topicoService: TopicoService) {
 
     @PutMapping
     @Transactional
+    @CacheEvict(value = ["topicos"], allEntries = true)
     fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView> {
         val topicoView = topicoService.atualizar(form)
         return ResponseEntity.ok(topicoView)
@@ -52,6 +58,7 @@ class TopicoController(private val topicoService: TopicoService) {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
+    @CacheEvict(value = ["topicos"], allEntries = true)
     fun deletar(@PathVariable id: Long) {
         topicoService.deletar(id)
     }
